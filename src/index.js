603a719356a306aeed9e379ef4c0e1f1366a5cb5
Melissa <lynch.melissat@gmail.com>
2023-02-15 14:34:09 -0500

    initialized BrowserRouter in index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './fonts/SpongeboyRegular.otf'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import './fonts/SpongeboyRegular.otf'
+import { BrowserRouter } from "react-router-dom";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
