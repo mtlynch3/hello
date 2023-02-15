8e1ff2d9f6b923d98316e09c838141221106cf49
Melissa <lynch.melissat@gmail.com>
2023-02-15 14:51:17 -0500

    defined routes and added navigation in App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,78 @@
 import './App.css';
-import Homepage from './components/Homepage';
 
-function App() {
+import Homepage from './components/Homepage';
+import Lodging from './components/Lodging';
+import Attractions from './components/Attractions';
+import Restaurants from './components/Restaurants';
+
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+
+
+export default function App() {
   return (
     <div className="App">
-      Welcome to Project 1 Demo! You can find the documentation and requirements for this assignment here. 
-      This project will cover the following tools for building UIs: React, CSS, and React-Router.
-      You will also be assessed on your use of git/GitHub and feature branch workflow.
-      See example.
-      <Homepage />
+      <h1>Welcome to Project One!</h1>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Homepage />} />
+          <Route path="lodging" element={<Lodging />} />
+          <Route path="restaurants" element={<Restaurants />} />
+          <Route path="attractions" element={<Attractions />} />
 
+
+          {/* Using path="*"" means "match anything", so this route
+                acts like a catch-all for URLs that we don't have explicit
+                routes for. */}
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
 
-export default App;
+function Layout() {
+  return (
+    <div>
+      {/* A "layout route" is a good place to put markup you want to
+          share across all the pages on your site, like navigation. */}
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/lodging">Lodging</Link>
+          </li>
+          <li>
+            <Link to="/restaurants">Restaurants</Link>
+          </li>
+          <li>
+            <Link to="/attractions">Attractions</Link>
+          </li>
+          <li>
+            <Link to="/nothing-here">Nothing Here</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <hr />
+
+      {/* An <Outlet> renders whatever child route is currently active,
+          so you can think about this <Outlet> as a placeholder for
+          the child routes we defined above. */}
+      <Outlet />
+    </div>
+  );
+}
+
+function NoMatch() {
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
+    </div>
+  );
+}
+
