3db495e1c1797f789e428d83d2a5184bd03a010b
Melissa <lynch.melissat@gmail.com>
2023-03-01 16:55:53 -0500

    row properties and align-items

--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -4,7 +4,10 @@ function Restaurants() {
   return (
     <div className="grid-examples">
       <div className="container">
-        <div className="item">Item 1</div>
+        <div className="item">
+          Hello here is a lot of text. One two three four five six seven eight nine ten.
+          Jack and the beanstalk. Fee fi fo fum. Hello, world! One day I will go to the moon.
+        </div>
         <div className="item">Item 2</div>
         <div className="item">Item 3</div>
         <div className="item">Item 4</div>
