8670e32f2bd952a202e83c4cfc683a0627b9ab8b
Melissa <lynch.melissat@gmail.com>
2023-03-01 14:23:21 -0500

    added initial setup for grid demo

--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -1,9 +1,19 @@
-//import './Restaurants.css';
+import './css/Restaurants.css';
 
 function Restaurants() {
   return (
-    <div className="content">
-        This page will have information about the Krusty Krab and the Chum Bucket.
+    <div className="grid-examples">
+      <div className="container">
+        <div className="item">Item 1</div>
+        <div className="item">Item 2</div>
+        <div className="item">Item 3</div>
+        <div className="item">Item 4</div>
+        <div className="item">Item 5</div>
+        <div className="item">Item 6</div>
+        <div className="item">Item 7</div>
+        <div className="item">Item 8</div>
+        <div className="item">Item 9</div>
+      </div>
     </div>
   );
 }
